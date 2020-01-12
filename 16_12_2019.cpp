6d5be9bf171f0178116daa566ad9c2ce8bb525c5
a-float <mati852456@gmail.com>
2020-01-12 23:29:28 +0100

    pointers and sorts

---
+++ 16_12_2019.cpp
@@ -0,0 +1,25 @@
+//stosy
+
+// init(st);
+// push(st,x);
+// pop(st); (=top()+pop())
+// empty(st);
+
+//tablicowa implementacja szybsza ale wymaga N
+
+struct Node{
+    int tab[100];
+    Node *next;
+}
+void init(stos/kolejka &st){
+	st.size = 0;
+}
+
+//kolejka
+
+//init(k);
+//put(k,x);
+//get(k);
+//empty(k);
+
+//poczatek kolejki 
